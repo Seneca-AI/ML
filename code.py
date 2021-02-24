88a8715ad66e22770c37a8fc2b55b180fe556f2b
absagargupta <absagargupta@gmail.com>
2021-02-24 22:20:49 +0530

    added main for marking lane changes and saving in csv

--- code.py
+++ code.py
@@ -86,9 +86,19 @@ for i in glob.glob(source_binary):
             break
 
 #%% deciding if the lanes exist in the area after cropping the area
-source_binary = "/media/sagar/New Volume/everything/job/Seneca/detecting_lane_changes/images/binary_masks/*"
-for i in glob.glob(source_binary):
+source_binary = "/media/sagar/New Volume/everything/job/Seneca/data/making_vid/binary_results/*"
+import pandas as pd
+names = []
+lane_change = []
+for i in sorted(glob.glob(source_binary)):
+    s = 0
     img = cv2.imread(i,0)
+    img = cv2.resize(img, (512,257), interpolation = cv2.INTER_AREA)
+    cv2.imshow("img", img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    break
+    
     X1 = int(img.shape[1]*1/4)+2
     X2 = int(img.shape[1]*3/4)
     Y1 = 211
@@ -98,13 +108,27 @@ for i in glob.glob(source_binary):
     element = [250,251,252,253,254,255]
     existing = np.isin(element, unique_array)
     print(i)
+    name = i.split("/")[-1]
     for j in existing:
         #print(i)
-        if j == False:
-            print("lane change did not happen")
-        elif j == True:
-            print("lane change has happened")
+        
+        if j == True:
+            s = 1
             break
+        elif j == False:
+            #s = 0
+            continue
+    names.append(name)
+    lane_change.append(s)
+dict = {'name_of_img': names, 'lane change (0/1)': lane_change}   
+   
+df = pd.DataFrame(dict)  
+    
+# saving the dataframe  
+df.to_csv('/media/sagar/New Volume/everything/job/Seneca/detecting_lane_changes/github/ML/Image_names_and_lane_change_status.csv')  
+    
+            
+            
  
 
 
