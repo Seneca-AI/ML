d4d882f74b17b57327c5e809afe8db4307c402aa
Luca409 <lucaloncar@gmail.com>
2021-04-10 15:10:54 -0400

    lane change code diff from main branch

--- utils/converting_video_to_frames.py
+++ utils/converting_video_to_frames.py
@@ -3,7 +3,7 @@ sample CLI:
 1. cd utils
 2. python3 converting_video_to_frames.py --source_vid /media/sagar/"New Volume"/everything/job/Seneca/data/making_vid/vids/clip2.avi --output_images /media/sagar/"New Volume"/everything/job/Seneca/data/making_vid/frame_extraction/
 Code Description
-Outputs the extracted frames from a given video file and outputs them to a specified directory.
+Making frames from video
 """
 import cv2
 import time
@@ -26,27 +26,24 @@ def video_to_frames(input_loc, output_loc):
     Returns:
         None
     """
-    # Warnings and errors
-    if not os.path.exists(input_loc):
-        raise ValueError("The required vid is not present. Please recheck the input vid source.")
-    
-    #Function starts
-    if not os.path.exists(output_loc):
+    try:
         os.mkdir(output_loc)
-        
+    except OSError:
+        pass
+    
     time_start = time.time()
-    video_capture = cv2.VideoCapture(input_loc)
-    frame_count = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
+    cap = cv2.VideoCapture(input_loc)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
     print ("Number of frames: ", frame_count)
     count = 0
     print ("Converting video..\n")
-    while video_capture.isOpened():
-        ret, frame = video_capture.read()
+    while cap.isOpened():
+        ret, frame = cap.read()
         cv2.imwrite(output_loc + "/%#05d.jpg" % (count+1), frame)
         count = count + 1
         if (count > (frame_count-1)):
             time_end = time.time()
-            video_capture.release()
+            cap.release()
             print ("Done extracting frames.\n%d frames extracted" % count)
             print ("It took %d seconds for conversion." % (time_end-time_start))
             break
